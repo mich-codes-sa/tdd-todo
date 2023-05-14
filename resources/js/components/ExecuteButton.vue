6b4f2ef0967592660b17c3414206f252df2432d3
Mich Stacey <77338457+mich-codes-sa@users.noreply.github.com>
2023-05-14 17:48:13 +0200

    Added e2e tests

---
+++ resources/js/components/ExecuteButton.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="flex basis-1/4 justify-center p-2.5">
+        <button
+            id="execute"
+            @click="$emit('newExecution')"
+            class="bg-primary-600 text-white font-semibold text-2xl px-4 py-2 rounded-lg"
+        >
+            +1
+        </button>
+    </div>
+</template>
+
+<script setup></script>
---
+++ tests/Unit/ExecuteButton.test.js
@@ -0,0 +1,21 @@
+import { describe, it, expect, beforeEach } from "vitest";
+import { mount } from "@vue/test-utils";
+import ExecuteButton from "@/components/ExecuteButton.vue";
+
+describe("ExecuteButton.vue", () => {
+    let wrapper = null;
+
+    beforeEach(() => {
+        wrapper = mount(ExecuteButton);
+    });
+
+    it("renders the button", () => {
+        expect(wrapper.find("#execute").text()).toBe("+1");
+    });
+
+    it("increments the executions", async () => {
+        await wrapper.find("#execute").trigger("click");
+
+        expect(wrapper.emitted("newExecution")).toBeTruthy();
+    });
+});
